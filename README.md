c5f5e66efb443d4e05571235af8e2eaf18325e8d
Vadim Zaliva <lord@crocodile.org>
2024-05-25 11:25:17 -0700

    apkg generation works

--- README.md
+++ README.md
@@ -5,7 +5,33 @@
   2. `vocab_sync.py` syncs the new words from `vocab.db` to
      `definitions.db`.
   3. `fetch_defs.py` fetches missing word definitions in
-     `definitions.db`. This is optional and requires OpenAI API key.
+     `definitions.db`. This is optional and requires an OpenAI API key.
+  4. `anki_export.py` exports `definitions.db` into Anki deck (.apkg file)
+
+  
+  Config file:
+  
+  ```
+  [openai]
+  key = <your open AI key>
+  
+  [anki]
+  deck_id = <your unique deck id>
+  deck_name = <name of your deck. E.g. "John's Kindle Vocabulary"
+
+  ```
+
+  You need an OpenAI API key to fetch definitions. It is only used by
+  `fetch_defs.py`.
+  
+  Anki `deck_id` uniquely identifies your personal deck. This will
+  allow you to re-import it when the new words were added and the deck
+  file was re-generated.  You can generate a unique `deck_id` using
+  the following command:
+  
+  ```
+  python3 -c "import random; print(random.randrange(1 << 30, 1 << 31))"
+  ```
 
 # Requirements
   * python3
@@ -21,6 +47,15 @@
   pip install -r requirements.txt
   ```
 
+# TODO
+  
+  - The cards are very simple now: just a word and definitions. It should be possible to add IPA,
+    usage examples, and audio.
+  - The use of OpenAI to fetch definitions may be controversial. An alternative definitions source,
+    such as Oxford Dictionaries API may be implemented. This was the reason we split definitions
+    fetching into a separate script.
+  
+
 ## Related
 https://github.com/wzyboy/kindle_vocab_anki
-https://github.com/smb-apache/yaktoa
+https://github.com/johan456789/yaktoa
---
+++ anki_export.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+
+import sys
+from os import path
+import sqlite3
+import click
+from icecream import ic
+import configparser
+import genanki
+
+# Unique ID of this model (schema)
+model_id=1684351491
+
+model = genanki.Model(
+  model_id,
+  'Kindle vocabulary builder',
+  fields=[
+    {'name': 'Word'},
+    {'name': 'Definition'},
+  ],
+  templates=[
+    {
+      'name': 'Card 1',
+      'qfmt': '{{Word}}',
+      'afmt': '{{FrontSide}}<hr id="answer">{{Definition}}',
+    },
+  ])
+
+@click.command()
+@click.option('--verbose', is_flag=True)
+@click.option('--config-file', default="config.cfg", help='config.cfg file')
+@click.option('--definitions-file', default="definitions.db", help='definitions data file')
+@click.option('--anki-file', default="myvocab.apkg", help='Anki deck file (will be overwritten)')
+def anki_export(verbose, config_file, definitions_file, anki_file):
+
+    config = configparser.ConfigParser()
+    config.read(config_file)
+
+    if path.exists(definitions_file):
+        if verbose:
+            print("Opening definitions `%s`" % definitions_file)
+    else:
+        print("Definitions file `%s` not found" % definitions_file)
+        exit(1)
+
+    deck_id = config['anki']['deck_id']
+    deck_name = config['anki']['deck_name']
+        
+    deck = genanki.Deck(int(deck_id), deck_name)
+
+    n = 0
+    try:
+       db = sqlite3.connect(definitions_file)
+       c = db.cursor()
+       c.execute("SELECT id, definition FROM DEFS WHERE definition_status = 1")
+       defs = c.fetchall()
+       for d in defs:
+           # Split the full_id into language code and word
+           full_id = d[0]
+           definition = d[1]
+           language_code, word = full_id.split(':')
+           if verbose:
+               print ("Exporting %s" % word)
+               
+           note = genanki.Note(
+               model = model,
+               fields = [word, definition])
+           n = n + 1
+           
+           deck.add_note(note)
+           
+       if verbose:
+           print ("Writing %s" % anki_file)
+           genanki.Package(deck).write_to_file(anki_file)
+
+    except Exception as e:
+        print("An error occurred. Export failed:", e)
+    finally:
+        c.close()
+        db.close()    
+        print("%d cards were added" % n)
+
+if __name__ == '__main__':
+    anki_export()
